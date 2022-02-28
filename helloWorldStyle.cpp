82c5116fac01479f8f400a4a5bf82e8b12d00ba9
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-02-28 14:33:04 +0000

    commit message

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) Year Your Name All rights reserved.
+
+//
+
+// Created by: Ferdaws
+
+// Date: 02/28/2022
+
+// It says helllo world and has a style ckecker.
+
+
+
+
+#include <iostream>
+int main()
+{std::cout << "Hello World" << std::endl;}
